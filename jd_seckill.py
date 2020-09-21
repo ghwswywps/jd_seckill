a2cae776e96e66ff64350ee8fd3870a20402b749
Flaki <i@abigant.com>
2020-09-21 17:06:15 +0800

    Update jd_seckill.py

--- jd_seckill.py
+++ jd_seckill.py
@@ -4,7 +4,27 @@
 import requests
 import time
 import json
+import os
+import copy
+import _thread
 
+config = {
+    '七彩虹 AD 3080':{
+        'thor':'01D9D4FEE370BB8557B74456D290C8C30F3F874F1F5C3E72C9C2CE53C83758B2A44F86328533DA69B8E1D16085AD363E28FD66831496AA34466DE7890685E280379A4F653CF2FF8BC3728CB4EBD4C0F80E500858AC3639D89AF2AA04F5E2A2EE0CB39BFE556FA1F5D9B9349F21D28BBF6A62DDE02CC80656AB6C0DC1EC6EA1AEF4E7E0CEFC9198A19A5A00185747A6DF3402E02B278333411D70D77EFA660B40',
+        'goods_url':'https://item.jd.com/10021776150443.html',
+        'order_time':'2020-09-21 20:30:00',
+    },
+    '七彩虹 水神 3080':{
+        'thor':'01D9D4FEE370BB8557B74456D290C8C30F3F874F1F5C3E72C9C2CE53C83758B2A44F86328533DA69B8E1D16085AD363E28FD66831496AA34466DE7890685E280379A4F653CF2FF8BC3728CB4EBD4C0F80E500858AC3639D89AF2AA04F5E2A2EE0CB39BFE556FA1F5D9B9349F21D28BBF6A62DDE02CC80656AB6C0DC1EC6EA1AEF4E7E0CEFC9198A19A5A00185747A6DF3402E02B278333411D70D77EFA660B40',
+        'goods_url':'https://item.jd.com/10021776150443.html',
+        'order_time':'2020-09-21 20:40:00',
+    }
+}
+
+LOG_TEMPLE_BLUE='\033[1;34m{}\033[0m '
+LOG_TEMPLE_RED='\033[1;31m{}\033[0m '
+LOG_TEMPLE_SUCCESS='\033[1;32mSUCCESS\033[0m '
+LOG_TEMPLE_FAILED='\033[1;31mFAILED\033[0m '
 
 class JD:
     headers = {
@@ -23,15 +43,12 @@ class JD:
         self.session = requests.session()
 
         # 配置项 只要填这5个 1.cookie 2.url 3.秒杀时间 4.秒杀总次数限制 5.秒杀请求间隙
-        self.thor = ''	
+        self.thor = '01D9D4FEE370BB8557B74456D290C8C30F3F874F1F5C3E72C9C2CE53C83758B2A44F86328533DA69B8E1D16085AD363E28FD66831496AA34466DE7890685E280379A4F653CF2FF8BC3728CB4EBD4C0F80E500858AC3639D89AF2AA04F5E2A2EE0CB39BFE556FA1F5D9B9349F21D28BBF6A62DDE02CC80656AB6C0DC1EC6EA1AEF4E7E0CEFC9198A19A5A00185747A6DF3402E02B278333411D70D77EFA660B40'	
         self.goods_url = 'https://item.jd.com/10021776150443.html'
         self.order_time = '2020-09-21 20:30:00'
         self.retry_limit = 20
         self.gap = 0.1
-
-
-        timeArray = time.strptime(self.order_time, "%Y-%m-%d %H:%M:%S")
-        self.order_time_st = int(time.mktime(timeArray))
+        
 
 
     def login(self): 
@@ -41,39 +58,81 @@ class JD:
         self.session.cookies.update(c)
         response = self.session.get(
             url=self.user_url, headers=JD.headers).text.strip('jsonpUserinfo()\n')
-        user_info = json.loads(response)
-        print('账号：', user_info.get('nickName'))
-        if user_info.get('nickName'):
-            while True:
-                time.sleep(self.gap)
-                if time.time() <= self.order_time_st:
-                    continue
-                try:
-                    if self.retry_limit < 1 :
-                        pass
-                    self.shopping()
-                    self.retry_limit = self.retry_limit - 1
-                except BaseException:
-                    continue
+        self.user_info = json.loads(response)
+        if self.user_info.get('nickName'):
+            for key in config:
+                item = copy.copy(self)
+                timeArray = time.strptime(config[key]['order_time'], "%Y-%m-%d %H:%M:%S")
+                order_time_st = int(time.mktime(timeArray))
+                item.order_time_st = order_time_st
+                config[key]['order_time_st'] = order_time_st
+                item.thor = config[key]['thor']
+                item.goods_url = config[key]['goods_url']
+                item.order_time = config[key]['order_time']
+                _thread.start_new_thread( self.run, (item, ))
                 pass
+            _thread.start_new_thread(self.log,())
+            pass
+            
+    def log(self):
+        clock = round(time.time())
+        while True:
+            time.sleep(0.1)
+            r = round(time.time())
+            if r > clock:
+                clock = r
+                print('\x1b[H\x1b[2J')
+                log = []
+                log.append(LOG_TEMPLE_BLUE.format('账号') + self.user_info.get('nickName') + '\n\n')
 
-    def shopping(self):
-        self.goods_id = self.goods_url[
-            self.goods_url.rindex('/') + 1:self.goods_url.rindex('.')]
-        JD.headers['referer'] = self.goods_url
-        buy_url = self.buy_url.format(self.goods_id)
-        self.session.get(url=buy_url, headers=JD.headers)  
+                for key in config:
+                    if time.time() <= config[key]['order_time_st']:
+                        log.append('\t\t' + LOG_TEMPLE_RED.format(key) + '\n')
+                        log.append(LOG_TEMPLE_BLUE.format('抢购时间')  + config[key]['order_time'] + '\n')
+                        log.append(LOG_TEMPLE_BLUE.format('剩余时间')  + str(round(config[key]['order_time_st'] - r, 2)) + '秒\n\n')
 
-        self.session.get(url=self.pay_url, headers=JD.headers) 
+                print(''.join(log))
+            
+            pass
+        pass
 
-        response = self.session.post(
-            url=self.pay_success, headers=JD.headers)     
-        print(response.text)
+    def run(self, item):
+        while True:
+            time.sleep(item.gap)
+            if time.time() <= item.order_time_st:
+                continue
+            try:
+                if item.retry_limit < 1 :
+                    pass
+                
+                item.shopping(item)
+                item.retry_limit = item.retry_limit - 1
+            except BaseException:
+                continue
+            pass
+        pass
+
+    def shopping(self, item):
+        item.goods_id = item.goods_url[
+            item.goods_url.rindex('/') + 1:item.goods_url.rindex('.')]
+        JD.headers['referer'] = item.goods_url
+        buy_url = item.buy_url.format(item.goods_id)
+        item.session.get(url=buy_url, headers=JD.headers)  
+
+        item.session.get(url=item.pay_url, headers=JD.headers) 
+
+        response = item.session.post(
+            url=item.pay_success, headers=JD.headers)
         order_id = json.loads(response.text).get('orderId')
         if order_id:
             print('抢购成功订单号:', order_id)
 
 jd = JD()
 jd.login()	
+
+while 1:
+    pass
+
+  
 
   
