138f129a9a115d86901faf20ea14f7ebbe48b279
Flaki <i@abigant.com>
2020-09-22 17:17:06 +0800

    Update jd_seckill.py

--- jd_seckill.py
+++ jd_seckill.py
@@ -7,13 +7,11 @@ import json
 import os
 import copy
 import _thread
+from bs4 import BeautifulSoup
+
+thor = ''
 
 config = {
-    '七彩虹 AD 3080':{
-        'thor':'',
-        'goods_url':'https://item.jd.com/100015062680.html',
-        'order_time':'2020-09-21 20:30:00',
-    }
 }
 
 LOG_TEMPLE_BLUE='\033[1;34m{}\033[0m '
@@ -37,8 +35,13 @@ class JD:
         self.goods_id = ''  
         self.session = requests.session()
 
+        self.rep_url = 'https://search.jd.com/search?keyword=3080&wq=3080&ev=24_95631%5E&shop=1&click=1'
+        self.g_url = 'https://item.jd.com/100015062658.html'
+        self.item_info_url = 'https://item-soa.jd.com/getWareBusiness?skuId={}'
+        self.appoint_url = ''
+
         # 配置项 只要填这5个 1.cookie 2.url 3.秒杀时间 4.秒杀总次数限制 5.秒杀请求间隙
-        self.thor = ''	
+        self.thor = thor
         self.goods_url = 'https://item.jd.com/10021776150443.html'
         self.order_time = '2020-09-21 20:30:00'
         self.retry_limit = 20
@@ -57,11 +60,10 @@ class JD:
         if self.user_info.get('nickName'):
             for key in config:
                 item = copy.copy(self)
-                timeArray = time.strptime(config[key]['order_time'], "%Y-%m-%d %H:%M:%S")
+                timeArray = time.strptime(config[key]['order_time'], "%Y-%m-%d %H:%M")
                 order_time_st = int(time.mktime(timeArray))
                 item.order_time_st = order_time_st
                 config[key]['order_time_st'] = order_time_st
-                item.thor = config[key]['thor']
                 item.goods_url = config[key]['goods_url']
                 item.order_time = config[key]['order_time']
                 _thread.start_new_thread( self.run, (item, ))
@@ -124,8 +126,58 @@ class JD:
             print('抢购成功订单号:', order_id)
             return True
 
+    def rep(self):
+        JD.headers['referer'] = 'https://cart.jd.com/cart.action'
+        c = requests.cookies.RequestsCookieJar()
+        c.set('thor', self.thor)  
+        self.session.cookies.update(c)
+        response = self.session.get(
+            url=self.user_url, headers=JD.headers).text.strip('jsonpUserinfo()\n')
+        self.user_info = json.loads(response)
+        p = self.session.get(url=self.rep_url, headers=JD.headers) 
+        bf = BeautifulSoup(p.text, features='html5lib')
+        texts = bf.find_all('div', class_ = 'p-name p-name-type-2') 
+        for text in texts:
+            time.sleep(0.2)
+            rtx = {}
+            rtx['title'] = text.em.text.replace('\n','')
+            rtx['goods_url'] = 'https:' + text.a['href']
+            jid = rtx['goods_url'] [rtx['goods_url'].rindex('/') + 1:rtx['goods_url'].rindex('.')]
+            p = self.session.get(url=self.item_info_url.format(jid), headers=JD.headers)
+            if p.text:
+                try:
+                    yyinfo = json.loads(p.text).get('yuyueInfo')
+                    if yyinfo:
+                        rtx['appoint_url'] = yyinfo['url']
+                        rtx['order_time'] = yyinfo['buyTime'].split('-202')[0]
+                        config[rtx['title']] = rtx
+                        print('正在添加如下商品：' + rtx['title'])
+                        pass
+                    pass
+                except BaseException:
+                    print(p.text)
+        self.appoint()
+
+
+    
+    def appoint(self):
+        print('开始预约\n')
+        for key in config:
+            ares = self.session.get(url='https:' + config[key]['appoint_url'], headers=JD.headers)
+            bf = BeautifulSoup(ares.text, features='html5lib')
+            texts = bf.find_all(class_ = 'bd-right-result')
+            if len(texts) > 0:
+                print(key + ' 预约结果：\n' + texts[0].text.strip())
+            else:
+                print(LOG_TEMPLE_RED.format(key + '\n需要手动预约：') + LOG_TEMPLE_BLUE.format('https:' + config[key]['appoint_url']))
+            print('--------------------------')  
+        input('输入任意按键继续...')
+
 jd = JD()
+jd.rep()
 jd.login()	
+
+
 
 while 1:
     pass
