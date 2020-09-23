bb3588c2778adb4fea77e001a36a777e5b3b047c
Flaki <i@abigant.com>
2020-09-23 12:29:53 +0800

    Update jd_seckill.py

--- jd_seckill.py
+++ jd_seckill.py
@@ -11,8 +11,6 @@ from bs4 import BeautifulSoup
 
 thor = ''
 
-config = {
-}
 
 LOG_TEMPLE_BLUE='\033[1;34m{}\033[0m '
 LOG_TEMPLE_RED='\033[1;31m{}\033[0m '
@@ -39,6 +37,7 @@ class JD:
         self.g_url = 'https://item.jd.com/100015062658.html'
         self.item_info_url = 'https://item-soa.jd.com/getWareBusiness?skuId={}'
         self.appoint_url = ''
+        self.config = {}
 
         # 配置项 只要填这5个 1.cookie 2.url 3.秒杀时间 4.秒杀总次数限制 5.秒杀请求间隙
         self.thor = thor
@@ -58,14 +57,14 @@ class JD:
             url=self.user_url, headers=JD.headers).text.strip('jsonpUserinfo()\n')
         self.user_info = json.loads(response)
         if self.user_info.get('nickName'):
-            for key in config:
+            for key in self.config:
                 item = copy.copy(self)
-                timeArray = time.strptime(config[key]['order_time'], "%Y-%m-%d %H:%M")
+                timeArray = time.strptime(self.config[key]['order_time'], "%Y-%m-%d %H:%M")
                 order_time_st = int(time.mktime(timeArray))
                 item.order_time_st = order_time_st
-                config[key]['order_time_st'] = order_time_st
-                item.goods_url = config[key]['goods_url']
-                item.order_time = config[key]['order_time']
+                self.config[key]['order_time_st'] = order_time_st
+                item.goods_url = self.config[key]['goods_url']
+                item.order_time = self.config[key]['order_time']
                 _thread.start_new_thread( self.run, (item, ))
                 pass
             _thread.start_new_thread(self.log,())
@@ -73,23 +72,29 @@ class JD:
             
     def log(self):
         clock = round(time.time())
+        i = 0
         while True:
             time.sleep(0.1)
             r = round(time.time())
             if r > clock:
+                i += 1
                 clock = r
                 print('\x1b[H\x1b[2J')
                 log = []
                 log.append(LOG_TEMPLE_BLUE.format('账号') + self.user_info.get('nickName') + '\n\n')
 
-                for key in config:
-                    if time.time() <= config[key]['order_time_st']:
+                for key in self.config:
+                    if time.time() <= self.config[key]['order_time_st']:
                         log.append('\t\t' + LOG_TEMPLE_RED.format(key) + '\n')
-                        log.append(LOG_TEMPLE_BLUE.format('抢购时间')  + config[key]['order_time'] + '\n')
-                        log.append(LOG_TEMPLE_BLUE.format('剩余时间')  + str(round(config[key]['order_time_st'] - r, 2)) + '秒\n\n')
+                        log.append(LOG_TEMPLE_BLUE.format('抢购时间')  + self.config[key]['order_time'] + '\n')
+                        log.append(LOG_TEMPLE_BLUE.format('剩余时间')  + str(round(self.config[key]['order_time_st'] - r, 2)) + '秒\n\n')
 
                 print(''.join(log))
-            
+                if i > 3600:
+                    print('开始扫描是否有新增3080商品...')
+                    self.rep()
+                    self.appoint()
+                    i = 0           
             pass
         pass
 
@@ -141,6 +146,8 @@ class JD:
             time.sleep(0.2)
             rtx = {}
             rtx['title'] = text.em.text.replace('\n','')
+            if rtx['title'] in self.config:
+                continue
             rtx['goods_url'] = 'https:' + text.a['href']
             jid = rtx['goods_url'] [rtx['goods_url'].rindex('/') + 1:rtx['goods_url'].rindex('.')]
             p = self.session.get(url=self.item_info_url.format(jid), headers=JD.headers)
@@ -150,36 +157,41 @@ class JD:
                     if yyinfo:
                         rtx['appoint_url'] = yyinfo['url']
                         rtx['order_time'] = yyinfo['buyTime'].split('-202')[0]
-                        config[rtx['title']] = rtx
+                        self.config[rtx['title']] = rtx
+                        rtx['appoint'] = False
                         print('正在添加如下商品：' + rtx['title'])
                         pass
                     pass
                 except BaseException:
                     print(p.text)
-        self.appoint()
 
 
     
     def appoint(self):
         print('开始预约\n')
-        for key in config:
-            ares = self.session.get(url='https:' + config[key]['appoint_url'], headers=JD.headers)
-            bf = BeautifulSoup(ares.text, features='html5lib')
-            texts = bf.find_all(class_ = 'bd-right-result')
-            if len(texts) > 0:
-                print(key + ' 预约结果：\n' + texts[0].text.strip())
-            else:
-                print(LOG_TEMPLE_RED.format(key + '\n需要手动预约：') + LOG_TEMPLE_BLUE.format('https:' + config[key]['appoint_url']))
-            print('--------------------------')  
-        input('输入任意按键继续...')
+        for key in self.config:
+            if not self.config[key]['appoint']:
+                ares = self.session.get(url='https:' + self.config[key]['appoint_url'], headers=JD.headers)
+                bf = BeautifulSoup(ares.text, features='html5lib')
+                texts = bf.find_all(class_ = 'bd-right-result')
+                if len(texts) > 0:
+                    print(key + ' 预约结果：\n' + texts[0].text.strip())
+                    self.config[key]['appoint'] = True
+                else:
+                    print(LOG_TEMPLE_RED.format(key + '\n需要手动预约：') + LOG_TEMPLE_BLUE.format('https:' + self.config[key]['appoint_url']))
+                print('--------------------------')  
+        for i in range(10):
+            time.sleep(1) 
+            end_str = '100%'
+            print('\r' + str(10 - i) + '秒后进入监控页面，若需要手动预约请CTRL+C退出脚本预约后再执行...', end='', flush=True)
+
 
 jd = JD()
 jd.rep()
+jd.appoint()
 jd.login()	
-
 
 
 while 1:
     pass
 
-  
