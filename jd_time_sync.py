f636ca4ee0d3af18cd4d9fef85e1856da61d4dc9
Su Yue <l@damenly.su>
2020-12-14 15:14:41 +0800

    添加linux 和 macos的时间同步，重命名jd_time_sync_win.py为jd_time_sync.py Signed-off-by: Su Yue <l@damenly.su>

--- jd_time_sync.py
+++ jd_time_sync.py
@@ -8,7 +8,12 @@ import time
 from datetime import datetime
 import requests
 import json
-import win32api
+import os
+
+from sys import platform
+
+if platform == 'win32' or platform == 'win64':
+    import win32api
 
 def getTime():
     url = 'https://a.jd.com//ajax/queryServerData.html'
@@ -17,7 +22,7 @@ def getTime():
     #print(float(js.get('serverTime'))/1000)
     return float(js.get('serverTime')/1000)
 
-def setSystemTime():
+def setSystemTimeWin():
     jd_time = getTime()
     tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst = time.gmtime(jd_time)
     #strTime = datetime.strftime(datetime.fromtimestamp(getTime()),'%Y-%m-%d %H:%M:%S.%f')
@@ -26,7 +31,21 @@ def setSystemTime():
     msec = int(float(datetime.strftime(datetime.fromtimestamp(jd_time),'%f'))/1000);
     print(strTime)
     print('msec：',msec)
-    win32api.SetSystemTime(tm_year, tm_mon, tm_wday, tm_mday, tm_hour, tm_min, tm_sec, msec)
+
+def setSystemTimeUnix():
+        cmd = "date -s @" + str(getTime())
+        #print(cmd)
+        os.system(cmd)
+
+def setSystemTime():
+    if platform == 'win32' or platform == 'win64':
+        setSystemTimeWin()
+    elif platform == 'linux' or platform == 'linux2' or platform == 'darwin':
+        setSystemTimeUnix()
+    else:
+        print("不支持时间同步：%s", platform)
+
+
 
 if __name__ == '__main__':
     setSystemTime()  #运行一次后，在本条语句前加#注释后可以查看时间同步情况
